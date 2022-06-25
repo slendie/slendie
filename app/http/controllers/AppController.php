ff1aa68f9049fb502aac7c71e9b470022609f418
lcloss <lcloss@gmail.com>
2022-06-25 11:30:07 +0100

    Remove Cards model

--- app/http/controllers/AppController.php
+++ app/http/controllers/AppController.php
@@ -2,7 +2,6 @@
 namespace App\Http\Controllers;
 
 use App\Controller;
-use App\Models\Card;
 
 use Slendie\Framework\Routing\Request;
 use Slendie\Framework\View\Template;
@@ -11,22 +10,6 @@ class AppController extends Controller
 {
     public function index()
     {
-        $cards = Card::all();
-
-        return view('index', compact('cards'));
-    }
-    public function about()
-    {
-        return view('about');
-    }
-    public function contact()
-    {
-        $view = new Template();
-        return $view->render('contact');
-        // return view('contact');
-    }
-    public function blog()
-    {
-        return view('blog');
+        return view('index');
     }
 }
