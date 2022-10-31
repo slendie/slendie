c34ed77a73be2f7512bc5765a8f2a1411951f1f4
lcloss <lcloss@gmail.com>
2022-10-31 22:37:25 +0000

    Functions

--- helpers/functions.php
+++ helpers/functions.php
@@ -1,76 +1,25 @@
 <?php
+use Slendie\Tools\Str;
 
 if ( !function_exists('startsWith') ) {
     function startsWith( $test, $subject ) {
-        if ( strlen($test) > strlen($subject) ) {
-            return false;
-        } else {
-            if ( substr( $subject, 0, strlen($test) ) === $test) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        return Str::startsWith( $test, $subject );
     }
 }
 
 if ( !function_exists('endsWith') ) {
     function endsWith( $test, $subject ) {
-        if ( strlen($test) > strlen($subject) ) {
-            return false;
-        } else {
-            $len = strlen($test);
-            $start = strlen($subject) - $len;
-            if ( substr( $subject, $start, $len ) == $test) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        return Str::endsWith( $test, $subject );
     }
 }
 
 if ( !function_exists('randomStr')) {
     function randomStr($length = 8, $params = ['lower', 'capital', 'number', 'symbol', 'simsym']) 
     {
-        $low_letters = "abcdefghijklmnopqrstuvwxyz";
-        $cap_letters = strtoupper($low_letters);
-        $symbols = "!?#$%&-_";
-        $simple_symbols = "-_";
-        $numbers = "1234567890";
-    
-        $source = '';
-        if ( in_array('lower', $params) ) {
-            $source .= $low_letters;
-        }
-        if ( in_array('capital', $params) ) {
-            $source .= $cap_letters;
-        }
-        if ( in_array('number', $params) ) {
-            $source .= $numbers;
-        }
-    
-        if ( in_array('symbol', $params) ) {
-            $source .= $symbols;
-    
-        } elseif ( in_array('simsymb', $params) ) {
-            $source .= $simple_symbols;
-        }
-    
-        $max = strlen($source);
-        $i = 0;
-        $word = "";
-    
-        while ($i < $length) {
-            $num = rand() % $max;
-            $char = substr($source, $num, 1);
-            $word .= $char;
-            $i++;
-        }
-    
-        return $word;
+        return Str::randomStr( $length, $params );
     }    
 }
+
 if ( !function_exists('dd') ) {
     function dd( ...$variables ) {
         echo '<pre>';
@@ -93,28 +42,6 @@ if ( !function_exists('dc') ) {
 if ( !function_exists('slugify') ) {
     function slugify($text, string $divider = '-')
     {
-        // replace non letter or digits by divider
-        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
-
-        // transliterate
-        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
-
-        // trim
-        $text = trim($text, $divider);
-
-        // remove duplicate divider
-        $text = preg_replace('~-+~', $divider, $text);
-
-        // lowercase
-        $text = strtolower($text);
-
-        if (empty($text)) {
-        return 'n-a';
-        }
-
-        return $text;
-    }    
+        return Str::slugify( $text, $divider );
+    }
 }
