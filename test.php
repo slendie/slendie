9818c71d8328ea5e9e2b83b43ef6923143aff752
lcloss <lcloss@gmail.com>
2022-12-03 19:19:27 +0000

    Testing

--- test.php
+++ test.php
@@ -1,11 +1,31 @@
 <?php
 
 use Slendie\Framework\Environment\Environment;
+use Slendie\Framework\View\Loader;
+use Slendie\Framework\View\Transpiler;
 
 include_once( 'bootstrap/app.php' );
 
+/* Test Environment */
 $env = Environment::getInstance();
 $env->load();
 $vars = $env->all();
+
+/* Test Loader */
+$loader = new Loader( 'view', 'packages/Slendie/Framework/tests', 'tpl.php' );
+$loader->parse();
+$sections = $loader->getSections();
+
+file_put_contents('loader.html', $loader->getContent());
+
+/* Test View Transpiler */
+$content = file_get_contents('packages/slendie/framework/tests/transpiler.tpl.php');
+
+$transpiler = new Transpiler( $content );
+$matches = $transpiler->parse();
+$transpiled = $transpiler->getContent();
+
+echo "Transpiled:\n";
+echo $transpiled;
 
 // dd( $vars );
